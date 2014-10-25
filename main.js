2dc88a2dda84f4ff52b317cfe6230e22730e2444
Aaron Dancer <me@aarondancer.com>
2014-10-24 23:11:45 -0500

    Fixed size issue, separated JS and CSS to files

---
+++ main.js
@@ -0,0 +1,67 @@
+var canvas, stage;
+        var drawingCanvas;
+        var oldPt;
+        var oldMidPt;
+        var color;
+        var stroke;
+        var index;
+
+        function init() {
+            if (window.top != window) {
+                document.getElementById("header").style.display = "none";
+            }
+            canvas = document.getElementById("theWall");
+            index = 0;
+
+            //check to see if we are running in a browser with touch support
+            stage = new createjs.Stage(canvas);
+            stage.autoClear = false;
+            stage.enableDOMEvents(true);
+
+            createjs.Touch.enable(stage);
+            createjs.Ticker.setFPS(60);
+
+            drawingCanvas = new createjs.Shape();
+
+            stage.addEventListener("stagemousedown", handleMouseDown);
+            stage.addEventListener("stagemouseup", handleMouseUp);
+
+            stage.addChild(drawingCanvas);
+            stage.update();
+
+            canvas.height = document.body.clientHeight;
+            canvas.width = document.body.clientWidth;
+        }
+
+        function stop() {}
+
+        function handleMouseDown(event) {
+            color = "#ff0000";
+            stroke = 5;
+            oldPt = new createjs.Point(stage.mouseX, stage.mouseY);
+            oldMidPt = oldPt;
+            stage.addEventListener("stagemousemove" , handleMouseMove);
+        }
+
+        function handleMouseMove(event) {
+            var midPt = new createjs.Point(oldPt.x + stage.mouseX>>1, oldPt.y+stage.mouseY>>1);
+
+            drawingCanvas.graphics.clear().setStrokeStyle(stroke, 'round', 'round').beginStroke(color).moveTo(midPt.x, midPt.y).curveTo(oldPt.x, oldPt.y, oldMidPt.x, oldMidPt.y);
+
+            oldPt.x = stage.mouseX;
+            oldPt.y = stage.mouseY;
+
+            oldMidPt.x = midPt.x;
+            oldMidPt.y = midPt.y;
+
+            stage.update();
+        }
+
+        function handleMouseUp(event) {
+            stage.removeEventListener("stagemousemove" , handleMouseMove);
+        }
+
+        window.onresize = function setCanvasSize(){
+            canvas.height = document.body.clientHeight;
+            canvas.width = document.body.clientWidth;
+        }
