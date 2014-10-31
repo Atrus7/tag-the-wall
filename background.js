7ef5eb0a5dbbffab14b21c05e9ddbfea68b64185
Aaron Dancer <me@aarondancer.com>
2014-10-31 03:55:08 -0500

    Created background.js
    Background.js handles storing the private ID, url, and whether or not
    to load private graffiti globally. Using a message listener
    background.js will return the values requested to back to the sender

--- background.js
+++ background.js
@@ -1,8 +1,18 @@
-﻿chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('window.html', {
-    'bounds': {
-      'width': 400,
-      'height': 500
-    }
-  });
+﻿var passed_id = "";
+var viewPrivate = false;
+var url = "";
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.greeting == "url")
+      sendResponse({farewell: url});
+  	if (request.greeting == "passed_id")
+      sendResponse({farewell: passed_id});
+  	if (request.greeting == "viewPrivate")
+      sendResponse({farewell: viewPrivate});
+  	if (request.greeting == "reseturl")
+      url = "";
+  	if (request.greeting == "resetpassed_id")
+      passed_id = "";
+  	if (request.greeting == "resetviewPrivate")
+      viewPrivate = false;
 });
