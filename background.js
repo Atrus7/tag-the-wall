1de3d07e9ac53da71c0f6805c571a0d4d2c9f2f1
Aaron Dancer <me@aarondancer.com>
2014-11-07 18:43:14 -0600

    Code formatting, more readable
    Just some basic code formatting to make things more readable

--- background.js
+++ background.js
@@ -2,17 +2,18 @@
 var viewPrivate = false;
 var url = "";
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.greeting == "url")
-      sendResponse({farewell: url});
-  	if (request.greeting == "passed_id")
-      sendResponse({farewell: passed_id});
-  	if (request.greeting == "viewPrivate")
-      sendResponse({farewell: viewPrivate});
-  	if (request.greeting == "reseturl")
-      url = "";
-  	if (request.greeting == "resetpassed_id")
-      passed_id = "";
-  	if (request.greeting == "resetviewPrivate")
-      viewPrivate = false;
-});
+	function(request, sender, sendResponse) {
+		if (request.greeting == "url")
+		  sendResponse({farewell: url});
+		if (request.greeting == "passed_id")
+		  sendResponse({farewell: passed_id});
+		if (request.greeting == "viewPrivate")
+		  sendResponse({farewell: viewPrivate});
+		if (request.greeting == "reseturl")
+		  url = "";
+		if (request.greeting == "resetpassed_id")
+		  passed_id = "";
+		if (request.greeting == "resetviewPrivate")
+		  viewPrivate = false;
+	}
+);
